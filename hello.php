d645ff4dc48a67435c7dfd01ee1570d1dc69072e
Henrik Romanenkov <henrrom@greeny.cs.tlu.ee>
2015-11-25 11:03:46 +0200

    added stuff

--- hello.php
+++ hello.php
@@ -1,6 +1,8 @@
 <?php require_once("header.php"); ?>
 
-
+	<!-- ###################### -->
+	<!-- ####### MENÜÜ ######## -->
+	<!-- ###################### -->
 
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
@@ -12,7 +14,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <a class="navbar-brand" href="#">Henrik</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -28,14 +30,38 @@
 
 <br></br><br></br><br></br><br></br><br></br>
     
-	<h1>Hello, world!</h1><h1>Hello, world!</h1>
-	<h1>Hello, world!</h1><h1>Hello, world!</h1>
-	<h1>Hello, world!</h1><h1>Hello, world!</h1>
-	<h1>Hello, world!</h1><h1>Hello, world!</h1>
+
+	<!-- ###################### -->
+	<!-- ####### SISU ######## -->
+	<!-- ###################### -->
 	
+<div class="container">
+
+
+	<div class="row">
+		<div class="col-sm-6 col-sm-push-6">
+			<ul class="nav nav-tabs">
+			  <li role="presentation" class="active"><a href="#">Home</a></li>
+			  <li role="presentation"><a href="#">Profile</a></li>
+			  <li role="presentation"><a href="#">Messages</a></li>
+			</ul>
+		</div>
 	
+		<div class="col-sm-6 col-sm-pull-6">
+			<div class="jumbotron">
+				<div class="container">
+				  <h1>Hello, world!</h1>
+				  <p>...</p>
+				  <p><a class="btn btn-success pull-right" href="#" role="bottom">Learn more</a></p>
+				</div>
+			</div>
+		</div>
+
 	
-	
+	</div>
+
+
+</div>
 	
 	
 <?php require_once("footer.php"); ?>
